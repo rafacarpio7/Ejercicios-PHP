107e9250f65762056dec2a4eae437a9504a9258e
rafacarpio7 <rafaeltredondo6@gmail.com>
2022-11-14 20:06:08 +0100

    dia 14

--- PUFOSA/log.php
+++ PUFOSA/log.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
-    <title>Document</title>
+    <title>PUFOSA - LOG</title>
 </head>
 <body>
     <?php
@@ -14,7 +14,7 @@
     $log = fopen("log.csv","rb");
 
 
-    echo "<table style='width: 400px;' ><th>Fichero LOG</th>";
+    echo "<table style='width: 400px;' ><th style='font-size: 28px';>Fichero LOG</th>";
     
 
     while (feof($log)==false) {
