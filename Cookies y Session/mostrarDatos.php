fba667e665388e7838e007632b2c2dd328b04e54
rafacarpio7 <rafaeltredondo6@gmail.com>
2023-01-19 20:22:37 +0100

    19/01/2023

--- Cookies y Session/mostrarDatos.php
+++ Cookies y Session/mostrarDatos.php
@@ -1,9 +1,10 @@
 <?php
-if (isset($_SESSION['usuario'])) {
-    session_start();
-    $_SESSION['usuario']=$_REQUEST['camareroId'];
-}
 
+session_start();
+echo session_id();
+echo "<pre>";
+print_r($_REQUEST['platos']);
+echo "</pre>";
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -33,36 +34,66 @@ try {
     <title>Document</title>
 </head>
 <body>
+    <a class="tag" href="logout.php"><button>Desconectar</button></a>
     <form action="" method="get">
         <?php
             foreach ($registros as $key ) {
-                echo "Plato de ".$key['nombre_plato']. " :".$key['precio']."€"."<input type='checkbox' name='platos' id='".$key['id']."' value='".$key['nombre_plato']."' >" ;
-                echo "<input type='hidden' name='idProducto' value='".$key['id']."'>";
+                echo "Plato de ".$key['nombre_plato']. " :".$key['precio']."€"."<input type='checkbox' name='platos[]' id='".$key['id']."' value='".$key['nombre_plato']."' ><br>" ;
+                
             }
         ?>
         <br>
         <input type="submit" name="btnAñadir" value="Añadir">
         <input type="submit" name="btnEliminar" value="Eliminar">
         <input type="submit" name="btnBorrarTodo" value="Borrar Todo">
-        <input type="submit" name="btnCerrarCuenta" value="Cerrar cuenta">
+        <input type="submit" name="btnCerrarCuenta" value="Cerrar cuenta"> 
         
     </form>
 
     <?php
-        if (isset($_REQUEST['btnAñadir'])) {
-            $_SESSION[$_REQUEST['idProducto']]=$_REQUEST['platos'];
-        }
-        echo $_REQUEST['platos']."<br>";
-        print_r($_SESSION);
+/*
+    try {
+        $conexion = new PDO("mysql:host=$servername;dbname=mi_restaurante_favorito;charset=utf8",$username,$password);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement =$conexion->prepare("SELECT id FROM platos  WHERE nombre_plato=:nombrePlato;");
+        $statement->bindParam('nombrePlato',$_REQUEST['platos']);
+        $statement->execute();
 
-        $arrayCuenta =array_count_values($_SESSION);
-        print_r($arrayCuenta);
+        $platos =$statement->fetchAll();
+
+        if (count($platos)>0) {
+           
+        }
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+*/
+
+
+
+
+    if (isset($_REQUEST['btnAñadir'])) {
+        foreach ($_REQUEST['platos'] as $key=>$value ) {
+            if (empty($_SESSION) || !array_key_exists($_REQUEST['platos'][$key],$_SESSION)) {
+                $_SESSION[$_REQUEST['platos'][$key]]=1;
+            }else {
+                $cantidadPlatos= $_SESSION[$value]+1;
+                $_SESSION[$_REQUEST['platos'][$key]]=$cantidadPlatos;
+            }
+        }
+    }
+
+
+    
+    print_r($_SESSION);
+
+
     ?>
 
     <div>
         <ul>
             <?php
-                foreach ($arrayCuenta as $key=>$value) {
+                foreach ($_SESSION as $key=>$value) {
                     echo "<li>". $key ." : " . $value.  "</li>";
                 }
             ?>
